95a9c292a5f875cfb2c6eb840e383fb802859c6f
Kr1612 <krupaligajbhiye123@gmail.com>
2024-12-29 21:01:31 +0530

    commit "arrow function"

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import Note from './Note'
+import React from "react";
+import Note from "./Note";
 
-function Background() {
+const Background = () => {
   return (
-    <div className='bg'>
-      <h1 className='bgtext'>ToDo</h1>
-      <div className='notebox'><Note/>
-      <Note/>
-      <Note/>
-      <Note/>
-
+    <div className="bg">
+      <h1 className="bgtext">ToDo</h1>
+      <div className="notebox">
+        <Note />
+        <Note />
+        <Note />
+        <Note />
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default Background
+export default Background;
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
+import React from "react";
 import { CiBoxList } from "react-icons/ci";
 import { FaRegStar } from "react-icons/fa";
 
-export default function Note() {
+const Note = () => {
   return (
-    <div className='mainnote'>
-      <h1 className='icon'><CiBoxList />
+    <div className="mainnote">
+      <h1 className="icon">
+        <CiBoxList />
       </h1>
-      <h5 className='text'>NOTE: each Icon package has it's own subfolder.</h5>
-        <div className='footer'>
-          <h4>Monday</h4>
-          <h4><FaRegStar />
+      <h5 className="text">NOTE: each Icon package has it's own subfolder.</h5>
+      <div className="footer">
+        <h4>Monday</h4>
+        <h4>
           <FaRegStar />
-          <FaRegStar /></h4>
-          
-        </div>
-      
+          <FaRegStar />
+          <FaRegStar />
+        </h4>
+      </div>
     </div>
-  )
-}
+  );
+};
+export default Note;
